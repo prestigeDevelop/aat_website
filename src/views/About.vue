ad42a90fa86b00e34cdb64ed17ee77b0f47f8b3e
prestigeDevelop <67553154+prestigeDevelop@users.noreply.github.com>
2020-12-28 21:46:07 -0400

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
-	<main class="container">
-		<section class="content">
+	<main class="content">
+		<!-- <section class="content">
 			<h5>About Me</h5>
-			<div class="right">
+			<div class="left">
 				<p>
 					I'm a believer that most humans grow and heal best by doing. Working with animals offers us a hands on, active
 					experience. Each animal, like humans has it's distinct personality, it provides us the needed space to project
@@ -17,7 +17,9 @@
 					my husband and daughter. Surrounded with loyal animal companions.
 				</p>
 			</div>
-			<img alt="aat" src="../assets/20180514_165336_edited.jpg" class="figure" />
+			<div class="right">
+				<img alt="aat" src="../assets/20180514_165336_edited.jpg" class="figure" />
+			</div>
 			<h4>Certifications:</h4>
 			<ul>
 				<li>Masters of Education and Leadership, Tel Aviv University</li>
@@ -27,21 +29,45 @@
 					International Society for Animal Assisted Therapy (ISAAT)
 				</li>
 			</ul>
-		</section>
-		<!-- <div class="some-page-wrapper">
-			<div class="row">
-				<div class="column">
-					<div class="blue-column">
-						Some Text in Column One
-					</div>
-				</div>
-				<div class="column">
-					<div class="green-column">
-						Some Text in Column Two
-					</div>
-				</div>
+		</section> -->
+		<div class="container">
+			<div class="container__text">
+				<h5>About Me</h5>
+				<p>
+					I'm a believer that most humans grow and heal best by doing. Working with animals offers us a hands on, active
+					experience. Each animal, like humans has it's distinct personality, it provides us the needed space to project
+					and analyze behaviours and situations while working on our goals.
+				</p>
+				<p>
+					Over the past 15 years I've worked with different age groups and organizations,such as schools, foster care,
+					department of social affairs, victims services, residential care, nursing homes and addiction units.
+				</p>
+				<p>
+					In 2015 I immigrated from Israel and am now a proud Canadian citizen. I currently live in Upper Tantallon with
+					my husband and daughter. Surrounded with loyal animal companions.
+				</p>
+				<h4>Certifications:</h4>
+				<ul>
+					<li>Masters of Education and Leadership, Tel Aviv University</li>
+					<li>
+						Diploma in Animal Assisted Therapy, Magid school for Animal Assisted Interventions of the Hebrew
+						University Jerusalem, recognized by International Association of Human-Animal Interaction
+						Organizations (IAHAIO) and International Society for Animal Assisted Therapy (ISAAT)
+					</li>
+				</ul>
 			</div>
-		</div> -->
+			<img class="container__image" src="../assets/20180514_165336_edited.jpg" />
+
+			<!-- <h4>Certifications:</h4>
+			<ul>
+				<li>Masters of Education and Leadership, Tel Aviv University</li>
+				<li>
+					Diploma in Animal Assisted Therapy, Magid school for Animal Assisted Interventions of the Hebrew University
+					Jerusalem, recognized by International Association of Human-Animal Interaction Organizations (IAHAIO) and
+					International Society for Animal Assisted Therapy (ISAAT)
+				</li>
+			</ul> -->
+		</div>
 	</main>
 </template>
 
@@ -53,56 +79,23 @@ export default {
 </script>
 <style lang="scss" scoped>
 .container {
+	//border: 1px solid blue;
+	//padding: 20px 40px 40px;
+	//max-width: 640px;
 	display: flex;
-	flex-grow: 1;
+	@media screen and (max-width: 992px) {
+		flex-direction: column;
+		//flex-wrap: wrap;
+	}
+	&__image {
+		margin: 0px 0px 0px 0px;
+		//flex: 1 1 0%;
+		max-width: 500px;
+		min-width: 100px;
+		object-fit: contain;
+	}
+	&__text {
+		flex: 1 1 0%;
+	}
 }
-
-.container > div {
-	border: 1px #42b983 solid;
-	flex: 1 1 0%;
-}
-
-// .contant {
-// 	display: flex;
-// 	flex-wrap: nowrap;
-// }
-.right {
-	flex: 1 1 0%;
-}
-
-.figure {
-	margin: 20px 30px 0 0;
-	width: 200px;
-	object-fit: contain;
-	align-self: flex-start;
-}
-
-//
-// .some-page-wrapper {
-// 	margin: 15px;
-// 	background-color: red;
-// }
-
-// .row {
-// 	display: flex;
-// 	flex-direction: row;
-// 	flex-wrap: wrap;
-// 	width: 100%;
-// }
-
-// .column {
-// 	display: flex;
-// 	flex-direction: column;
-// 	flex-basis: 100%;
-// }
-
-// .blue-column {
-// 	background-color: blue;
-// 	height: 100px;
-// }
-
-// .green-column {
-// 	background-color: green;
-// 	height: 100px;
-// }
 </style>
