ac1d87f6eba76e7416bfa314fb5b708c89e6fcab
prestigeDevelop <67553154+prestigeDevelop@users.noreply.github.com>
2021-01-03 20:32:18 -0400

    fix About page for Safari

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-	<main class="content">
-		<!-- <section class="content">
+	<!--<main class="content">
+		 <section class="content">
 			<h5>About Me</h5>
 			<div class="left">
 				<p>
@@ -29,44 +29,43 @@
 					International Society for Animal Assisted Therapy (ISAAT)
 				</li>
 			</ul>
-		</section> -->
-		<div class="container">
-			<div class="container__text">
-				<h5>About Me</h5>
-				<p>
-					I'm a believer that most humans grow and heal best by doing. Working with animals offers us a hands on, active
-					experience. Each animal, like humans has it's distinct personality, it provides us the needed space to project
-					and analyze behaviours and situations while working on our goals.
-				</p>
-				<p>
-					Over the past 15 years I've worked with different age groups and organizations,such as schools, foster care,
-					department of social affairs, victims services, residential care, nursing homes and addiction units.
-				</p>
-				<p>
-					In 2015 I immigrated from Israel and am now a proud Canadian citizen. I currently live in Upper Tantallon with
-					my husband and daughter. Surrounded with loyal animal companions.
-				</p>
-			</div>
-			<div class="right">
-				<img
-					class="container__image"
-					alt="Yana with Gucci and Churchill"
-					title="Yana with Gucci and Churchill"
-					src="../assets/20180514_165336_edited.jpg"
-				/>
-
-				<ul>
-					<h4>Certifications:</h4>
-					<li>Masters of Education and Leadership, Tel Aviv University</li>
-					<li>
-						Diploma in Animal Assisted Therapy, Magid school for Animal Assisted Interventions of the Hebrew
-						University Jerusalem, recognized by International Association of Human-Animal Interaction
-						Organizations (IAHAIO) and International Society for Animal Assisted Therapy (ISAAT)
-					</li>
-				</ul>
-			</div>
+		</section> </main>-->
+	<div class="container">
+		<div class="container__text">
+			<h5>About Me</h5>
+			<p>
+				I'm a believer that most humans grow and heal best by doing. Working with animals offers us a hands on, active
+				experience. Each animal, like humans has it's distinct personality, it provides us the needed space to project and
+				analyze behaviours and situations while working on our goals.
+			</p>
+			<p>
+				Over the past 15 years I've worked with different age groups and organizations,such as schools, foster care,
+				department of social affairs, victims services, residential care, nursing homes and addiction units.
+			</p>
+			<p>
+				In 2015 I immigrated from Israel and am now a proud Canadian citizen. I currently live in Upper Tantallon with my
+				husband and daughter. Surrounded with loyal animal companions.
+			</p>
 		</div>
-	</main>
+		<div class="container__left">
+			<img
+				class="container__left__image"
+				alt="Yana with Gucci and Churchill"
+				title="Yana with Gucci and Churchill"
+				src="../assets/20180514_165336_edited.jpg"
+			/>
+
+			<ul>
+				<h4>Certifications:</h4>
+				<li>Masters of Education and Leadership, Tel Aviv University</li>
+				<li>
+					Diploma in Animal Assisted Therapy, Magid school for Animal Assisted Interventions of the Hebrew University
+					Jerusalem, recognized by International Association of Human-Animal Interaction Organizations (IAHAIO) and
+					International Society for Animal Assisted Therapy (ISAAT)
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -82,30 +81,26 @@ export default {
 	//max-width: 640px;
 	display: flex;
 	flex-direction: column;
-	@media screen and (max-width: 992px) {
-		flex-direction: column;
-		//flex-wrap: wrap;
-	}
-	&__image {
-		margin: 0px 0px 0px 0px;
-		flex: 1 1 0%;
-		max-width: 500px;
-		//min-width: 50px;
-		object-fit: contain;
-	}
+
 	&__text {
 		flex: 1 1 0%;
 	}
-}
+	&__left {
+		display: flex;
 
-.right {
-	display: flex;
-	@media screen and (max-width: 992px) {
-		flex-direction: column;
-		//flex-wrap: wrap;
-	}
-	&__div {
-		flex: 1 1 0%;
+		@media screen and (max-width: 992px) {
+			flex-wrap: wrap;
+		}
+		&__div {
+			flex: 1 1 0%;
+		}
+		&__image {
+			margin: 0px 0px 0px 0px;
+			flex: 1 1 0%;
+			max-width: 500px;
+			align-self: flex-start;
+			object-fit: contain;
+		}
 	}
 }
 </style>
